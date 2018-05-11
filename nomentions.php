a499ca4c1f854f7ad7b6474232cf3fa8c0989a2b
Colin Walker <colin@colinwalker.blog>
2018-05-11 23:22:41 +0100

    Add files via upload

---
+++ nomentions.php
@@ -0,0 +1,38 @@
+<?php
+
+	if( !defined('ABSPATH') ) exit; //Don't run if accessed directly
+
+/**
+ *  Nomentions
+ *
+ *  @package Nomentions
+ *
+ *  Plugin Name: Nomentions
+ *
+ *
+ *  Description: Exclude links from webmentions
+ *
+ *  Requires: Webmention
+ *
+ *  Version: 0.1
+ *
+ *  Author: Colin Walker
+ *
+*/
+
+	function nomention_links( $links, $post_id ) {
+		$post = get_post( $post_id );
+		preg_match_all('/<a[^>]+>/i',$post->post_content, $results);
+    	$mentions = '';
+		foreach ($results[0] as $link) {
+  			if (!strpos($link, 'class="nomention')) {
+    			$mentions .= $link;
+  			}
+		}
+		$links = wp_extract_urls($mentions);
+		return $links;
+	}
+
+	add_filter( 'webmention_links', 'nomention_links', 10, 2 );
+
+?>
